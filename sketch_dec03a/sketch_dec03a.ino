362d5065b8ea4dd2281b2cd8f8c4a0c3b957a7f8
hwaaa <hshwaa@gmail.com>
2018-12-03 17:41:55 +0900

    아두이노 첫 번째 파일
    수정 한 번 했음!

--- sketch_dec03a/sketch_dec03a.ino
+++ sketch_dec03a/sketch_dec03a.ino
@@ -7,7 +7,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(led, HIGH);
-  delay(1000);
+  delay(500);
   digitalWrite(led, LOW);
   delay(1000);
 }
